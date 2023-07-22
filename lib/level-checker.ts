17f5ad98b68ce2bd0029a22e42d7a6b36128883f
Nooc <nooc@nooc.ink>
2023-07-22 14:34:13 +0800

    Attempt to fix errors

--- lib/level-checker.ts
+++ lib/level-checker.ts
@@ -4,14 +4,19 @@ import { cookies } from "next/headers";
 export const PAST_LEVEL_KEY = "passed-level";
 
 export function validateLevel(currentLevel: number): number {
-  const passedLevel = Number(cookies().get(PAST_LEVEL_KEY)?.value) ?? 0;
+  let passedLevel = Number(cookies().get(PAST_LEVEL_KEY)?.value) ?? 0;
+  passedLevel = Number.isNaN(passedLevel) ? 0 : passedLevel;
   console.log("validating level", currentLevel, passedLevel);
 
   if (currentLevel <= 0) {
     return passedLevel ?? 0;
   }
 
-  if (passedLevel === undefined || currentLevel > passedLevel + 1) {
+  if (
+    passedLevel === undefined ||
+    Number.isNaN(passedLevel) ||
+    currentLevel > passedLevel + 1
+  ) {
     console.log("redirecting", !passedLevel, currentLevel > passedLevel + 1);
     redirect("/");
   }
