7a492e8e163606b8f9fea217aa39e14618a6314c
Nooc <nooc@nooc.ink>
2023-07-22 20:20:57 +0800

    Update

--- app/fake/page.tsx
+++ app/fake/page.tsx
@@ -1,3 +1,17 @@
+import Image from "next/image";
+import dogImage from "./dog.png";
+
 export default function FakePage() {
-  return <div>你被骗了！继续寻找吧！</div>;
+  return (
+    <div className="flex flex-col">
+      <Image
+        className="aspect-square rounded-full p-10 object-cover"
+        src={dogImage}
+        alt="狗狗照片"
+      />
+      <div className="font-bold text-lg text-center">
+        这是一个假的碎片，你离真相已经很接近了，加油！就在附近！
+      </div>
+    </div>
+  );
 }
