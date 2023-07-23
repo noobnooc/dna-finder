e7484fd0b64f8f36b170878e484fbc6d98164a9e
Nooc <nooc@nooc.ink>
2023-07-23 15:16:12 +0800

    Update

--- app/fake/page.tsx
+++ app/fake/page.tsx
@@ -9,6 +9,9 @@ export default function FakePage() {
         src={dogImage}
         alt="狗狗照片"
       />
+      <div className="text-2xl mb-4 font-bold text-center mt-10 text-red-500">
+        很抱歉！
+      </div>
       <div className="font-bold text-lg text-center">
         这是一个假的碎片，你离真相已经很接近了，加油！就在附近！
       </div>
